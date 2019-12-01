cea3370ac568d650a9e7b34dfb75495edcd220be
Durga Prasad <ch.prasad893@gmail.com>
2019-12-01 14:51:45 +0530

    Updates

---
+++ precache-manifest.fb893cc50394dbae27b3baf70ec07834.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b983a7c1ece5fcd75325bdd1a04e0bb1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6d2c85695d2e56e55017",
+    "url": "/monsters-rolodex/static/css/main.dff19a83.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monsters-rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "6d2c85695d2e56e55017",
+    "url": "/monsters-rolodex/static/js/main.b0a17e24.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  }
+]);
